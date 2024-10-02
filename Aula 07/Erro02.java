9053319932dbf3cb00863b85dbe2c626108d27b5
João Victor Medina <joaovictor.zipp@gmail.com>
2024-10-02 16:21:57 -0300

    add aula 7 erros

---
+++ Aula 07/Erro02.java
@@ -0,0 +1,9 @@
+class Erro02 {
+	
+	public static void main (String[] args){
+
+		var int cont;
+
+		System.out.println("Você digitou: " + cont);
+	}
+}
---
+++ Aula 07/Erro03.java
@@ -0,0 +1,8 @@
+class Erro03 {
+	
+	public static void Main (String[] args){
+
+		//System.out.println("Você digitou: ");
+		System.out.println("Você digitou: " + 1/0);
+	}
+}
