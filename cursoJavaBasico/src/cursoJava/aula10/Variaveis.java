bdd3feb4b109abce4c099822e01de46df041941c
João Victor Medina <joaovictor.zipp@gmail.com>
2024-10-04 11:31:37 -0300

    add aula 10 variáveis

---
+++ cursoJavaBasico/src/cursoJava/aula10/Variaveis.java
@@ -0,0 +1,20 @@
+package cursoJava.aula10;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+	
+		int idade = 20;
+		String nome = "João";
+				
+		String nomeDoMeuCachorro = "totó";
+		
+		idade = 34;
+		
+		System.out.println("Idade = " + idade);
+		System.out.println("Nome = " + nome);
+		
+
+	}
+
+}
