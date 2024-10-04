e153ef553e3327913e99bde42d695892bcf12231
João Victor Medina <joaovictor.zipp@gmail.com>
2024-10-04 13:52:55 -0300

    add aula 11 variáveis

---
+++ cursoJavaBasico/src/cursojava/aula11/VariaveisChar.java
@@ -0,0 +1,19 @@
+package cursojava.aula11;
+
+public class VariaveisChar {
+
+	public static void main(String[] args) {
+		
+		//char o = 'o';
+				
+		//char i = 'i';
+		
+		char o = 111;
+		char i = 105;
+		char interrogacao = 0x00E1; //Valor é o ponto de interrogação = ?
+		
+		System.out.println(""+o+i+interrogacao);
+
+	}
+
+}
